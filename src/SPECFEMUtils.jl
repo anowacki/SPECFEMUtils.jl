5bdc8633e83c38a1f415320a3116f4e90e2a910c
Andy Nowacki <a.nowacki@leeds.ac.uk>
2019-02-25 22:23:43 +0000

    Update read_cmtsolution; add read_par_file
    - read_cmtsolution() now works.
    - read_par_file returns a Dict of keys and values in a Par_file.

--- src/SPECFEMUtils.jl
+++ src/SPECFEMUtils.jl
@@ -1,11 +1,12 @@
 """
 # SPECFEMUtils
 
-A number of utilities for seismic wave propagation simulations with SPECFEM3D_GLOBE.
+A number of utilities for dealing with seismic wave propagation simulations
+with the SPECFEM3D_GLOBE software.
 """
 module SPECFEMUtils
 
-import DelimitedFileds: readdlm
+import DelimitedFiles: readdlm
 
 import MomentTensors: MT
 
@@ -35,7 +36,7 @@ function read_cmtsolution(file)
     lat = parse(Float64, split(lines[5])[2])
     lon = parse(Float64, split(lines[6])[2])
     dep = parse(Float64, split(lines[7])[2])
-    mt = MT(parse(Float64, split(lines[i])[2]) for i in 8:13)
+    mt = MT([parse(Float64, split(lines[i])[2]) for i in 8:13])
     CMTSolution(desc, name, tshift, halfdur, lat, lon, dep, mt)
 end
 
@@ -47,6 +48,36 @@ Read in a STATIONS file and return a named tuple containing vectors of the stati
 function read_stations(file)
     d = readdlm(file)
     (sta=d[:,1], net=d[:,2], lat=d[:,3], lon=d[:,4], elev=d[:,5], dep=d[:,6])
+end
+
+"""
+    read_par_file(file) -> parameters
+    
+Read in a Par_file and return a dictionary `parameters` of the values present.
+"""
+function read_par_file(file)
+    params = Dict{String,Any}()
+    for (i, line) in enumerate(readlines(file))
+        length(replace(line, r"\s"=>"")) > 0 || continue
+        # Skip comment lines
+        occursin(r"^ *#", line) && continue
+        occursin(r"^.*=.*$", line) || error("Bad format of line $i: \"$line\"")
+        key = strip(replace(line, r"=.*"=>""))
+        value = strip(split(replace(line, r".*="=>""))[1])
+        value = if lowercase(value) == ".true."
+            true
+        elseif lowercase(value) == ".false."
+            false
+        elseif tryparse(Int, value) != nothing
+            parse(Int, value)
+        elseif tryparse(Float64, replace(value, r"[dD]"=>"e")) != nothing
+            parse(Float64, replace(value, r"[dD]"=>"e"))
+        else
+            value
+        end
+        params[key] = value
+    end
+    params
 end
 
 end # module
